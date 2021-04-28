3be7d26d0db49813def8c553f339a7a1f73a5435
aa3122 <78981950+aa3122@users.noreply.github.com>
2021-04-28 17:29:59 -0400

    Add files via upload

---
+++ Alphavantage.py
@@ -0,0 +1,20 @@
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+import mysql.connector
+import pymysql
+
+
+API_key = 'GH1BFTXNR1TJHCBQ'
+
+ts = TimeSeries(key = API_key,output_format='pandas')
+
+data = ts.get_daily_adjusted('MSFT')
+print(data[0])
+
+def connect():
+    mydb = mysql.connector.connect(
+        host="satoshi.cis.uncw.edu",
+        user="aa3122",
+        password="sDDOa4YpP"
+    )
+connect()
