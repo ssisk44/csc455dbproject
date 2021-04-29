8282b202dac1db50c02b8aa8eb1a8eb36d9ed223
ssisk44 <65787613+ssisk44@users.noreply.github.com>
2021-04-28 20:11:34 -0400

    This supplies a retrieve information function that converts the api call --> dataframe --> list for data value entry

--- Alphavantage.py
+++ Alphavantage.py
@@ -1,14 +1,31 @@
+from pprint import pprint
+
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
+import matplotlib.pyplot as plt
 import mysql.connector
 
 
+#VARIABLES
 API_key = 'GH1BFTXNR1TJHCBQ'
 
-ts = TimeSeries(key = API_key,output_format='pandas')
 
-data = ts.get_daily_adjusted('MSFT')
-print(data[0])
+#FUNCTIONS
+def main():
+    connect()
+    ts = TimeSeries(key = API_key,output_format='pandas')
+    df, metadata = ts.get_daily_adjusted(symbol='MSFT')
+    valuelist = retrieve_information(df)
+
+
+def retrieve_information(df):
+    big_list = []
+    for i in range(0,len(df)):
+        small_list = []
+        for j in range(0,8):
+            small_list.append(df.iloc[i][j])
+        big_list.append(small_list)
+    return big_list
 
 def connect():
     mydb = mysql.connector.connect(
@@ -17,4 +34,8 @@ def connect():
         password="sDDOa4YpP",
         database="CSC455SP21Finance"
     )
-connect()
+
+main()
+
+
+
