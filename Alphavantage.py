f149f7ecb0ac0472232ebc3f4887d85ba2905b52
ssisk44 <65787613+ssisk44@users.noreply.github.com>
2021-04-28 20:36:42 -0400

    partitioned code into functions to make easier to call on a mass scale later
    created pyodbc connection to use active sql queries insert into our db

--- Alphavantage.py
+++ Alphavantage.py
@@ -1,31 +1,34 @@
-from pprint import pprint
-
+####IMPORTS
+import pyodbc
 from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 import matplotlib.pyplot as plt
 import mysql.connector
 
 
-#VARIABLES
+#################################################################################
+#                                                                               #
+#                              TO-DO                                            #
+#                                                                               #
+#                                                                               #
+#                                                                               #
+#                                                                               #
+#################################################################################
+
+
+####VARIABLES
 API_key = 'GH1BFTXNR1TJHCBQ'
+stockTicker = 'MSFT'
 
 
-#FUNCTIONS
+
+####FUNCTIONS
 def main():
     connect()
-    ts = TimeSeries(key = API_key,output_format='pandas')
-    df, metadata = ts.get_daily_adjusted(symbol='MSFT')
-    valuelist = retrieve_information(df)
-
-
-def retrieve_information(df):
-    big_list = []
-    for i in range(0,len(df)):
-        small_list = []
-        for j in range(0,8):
-            small_list.append(df.iloc[i][j])
-        big_list.append(small_list)
-    return big_list
+    df = API_CALL()
+    cnxn = pyodbc.connect(df)  #creates a connection for python SQL queries https://towardsdatascience.com/sql-server-with-python-679b8dba69fa
+    valuelist = dataframe_to_list(df) #makes list for easy iterable variable allocation when we insert into database
+    print(valuelist)
 
 def connect():
     mydb = mysql.connector.connect(
@@ -34,6 +37,28 @@ def connect():
         password="sDDOa4YpP",
         database="CSC455SP21Finance"
     )
+
+def API_CALL():
+    ts = TimeSeries(key=API_key, output_format='pandas')
+    df, metadata = ts.get_daily_adjusted(symbol=stockTicker)
+    return df
+
+def setStockTicker(str):
+    stockTicker = str
+
+def dataframe_to_list(df):
+    big_list = []
+    for i in range(0,len(df)):
+        small_list = []
+        for j in range(0,8):
+            small_list.append(df.iloc[i][j])
+        big_list.append(small_list)
+    return big_list
+
+def formatDataEntry():
+    #nothing yet
+    return None
+
 
 main()
 
