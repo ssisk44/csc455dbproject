dcf5e6f6ae527fa5cc6a08ecf993f5cb33c6ea64
aa3122 <78981950+aa3122@users.noreply.github.com>
2021-04-29 20:36:09 -0400

    index.php creation and other php stuff

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "satoshi.cis.uncw.edu";
+$dbUsername = "aa3122";
+$dbPassword = "sDDOa4YpP";
+$dbName = "CSC455SP21Finance";
+
+$conn = mysqli_connects($dbServername, $dbUsername, $dbPassword, $dbName);
