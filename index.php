5c4e8f4f20a842acc0c473197f86a1f8f499f52e
aa3122 <78981950+aa3122@users.noreply.github.com>
2021-05-01 20:10:52 -0400

    index.php uppdate

--- index.php
+++ index.php
@@ -6,18 +6,26 @@
   <head>
       <title>Welcome to our CSC455 Final Project</title>
   </head>
+  <style>
+	table, th{
+		border: 2px solid black;}
+	td{
+		border: 1px solid green;}
+</style>
   <body>
-
+    <h2>PriceID LowPrice, and HighPrice</h2>
     <?php
       $sql = "SELECT * FROM priceRecord;";
       $result = mysqli_query($conn, $sql);
       $resultCheck = mysqli_num_rows($result);
 
       if ($resultCheck > 0) {
+	echo "<table><tr><th>PriceId</th><th>LowPrice</th><th>HighPrice</th></tr>";
         while ($row = mysqli_fetch_assoc($result)) {
-          echo $row['priceId'] . $row['lowPrice'] . "<br>";
-        }
+          echo "<tr><td>" . $row['priceId'] . "</td><td>" . $row['lowPrice']  . "</td><td>" . $row['highPrice'] . "</tr>";
       }
+	echo "</table>";
+}
     ?>
   </body>
 </html>
