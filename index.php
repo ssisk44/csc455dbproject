be1976b1f93cb9d3296ee55f9e62c1b1112dbae5
aa3122 <78981950+aa3122@users.noreply.github.com>
2021-05-03 21:13:47 -0400

    index with working min/mx query

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		border: 1px solid green;}
 </style>
   <body>
-    <h2>PriceID LowPrice, and HighPrice</h2>
+    <h2>Minimum Price For Each Stock</h2>
     <?php
       $sql = "SELECT MIN(priceRecord.endPrice), time.stockTicker, stock.companyName
 FROM ((priceRecord INNER JOIN time ON priceRecord.priceId = time.priceId) INNER JOIN stock ON time.stockTicker = stock.stockTicker)
@@ -24,10 +24,26 @@ GROUP BY time.stockTicker;";
       if ($resultCheck > 0) {
 	echo "<table><tr><th>Minimum Price</th><th>Stock Ticker</th><th>Company Name</th></tr>";
         while ($row = mysqli_fetch_assoc($result)) {
-          echo "<tr><td>" . $row['MIN(priceRecord.endPrice)'] . "</td><td>" . $row['time.stockTicker']  . "</td><td>" . $row['stock.companyName'] . "</tr>";
+          echo "<tr><td>" . $row['MIN(priceRecord.endPrice)'] . "</td><td>" . $row['stockTicker']  . "</td><td>" . $row['companyName'] . "</tr>";
+      }
+	echo "</table>";
+}?>
+<h2>Maximum Price For Each Stock</h2>
+<?php
+      $sql2 = "SELECT MAX(priceRecord.endPrice), time.stockTicker, stock.companyName
+FROM ((priceRecord INNER JOIN time ON priceRecord.priceId = time.priceId) INNER JOIN stock ON time.stockTicker = stock.stockTicker)
+GROUP BY time.stockTicker;";
+      $result2 = mysqli_query($conn, $sql2);
+      $resultCheck2 = mysqli_num_rows($result2);
+
+      if ($resultCheck2 > 0) {
+	echo "<table><tr><th>Maximum Price</th><th>Stock Ticker</th><th>Company Name</th></tr>";
+        while ($row = mysqli_fetch_assoc($result2)) {
+          echo "<tr><td>" . $row['MAX(priceRecord.endPrice)'] . "</td><td>" . $row['stockTicker']  . "</td><td>" . $row['companyName'] . "</tr>";
       }
 	echo "</table>";
 }
+
     ?>
   </body>
 </html>
