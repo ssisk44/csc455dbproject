25ac6ac67da6dffa84fa2230a83d4955b10f92db
aa3122 <78981950+aa3122@users.noreply.github.com>
2021-05-03 21:02:37 -0400

    index.php update

--- index.php
+++ index.php
@@ -15,14 +15,16 @@
   <body>
     <h2>PriceID LowPrice, and HighPrice</h2>
     <?php
-      $sql = "SELECT * FROM priceRecord;";
+      $sql = "SELECT MIN(priceRecord.endPrice), time.stockTicker, stock.companyName
+FROM ((priceRecord INNER JOIN time ON priceRecord.priceId = time.priceId) INNER JOIN stock ON time.stockTicker = stock.stockTicker)
+GROUP BY time.stockTicker;";
       $result = mysqli_query($conn, $sql);
       $resultCheck = mysqli_num_rows($result);
 
       if ($resultCheck > 0) {
-	echo "<table><tr><th>PriceId</th><th>LowPrice</th><th>HighPrice</th></tr>";
+	echo "<table><tr><th>Minimum Price</th><th>Stock Ticker</th><th>Company Name</th></tr>";
         while ($row = mysqli_fetch_assoc($result)) {
-          echo "<tr><td>" . $row['priceId'] . "</td><td>" . $row['lowPrice']  . "</td><td>" . $row['highPrice'] . "</tr>";
+          echo "<tr><td>" . $row['MIN(priceRecord.endPrice)'] . "</td><td>" . $row['time.stockTicker']  . "</td><td>" . $row['stock.companyName'] . "</tr>";
       }
 	echo "</table>";
 }
